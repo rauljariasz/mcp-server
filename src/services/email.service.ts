7a52e6e3913f5ca30b14b357d5f3be357c8f7288
rauljariasz <rauljariasz@gmail.com>
2024-06-20 17:00:55 -0400

    mailer

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -4,7 +4,7 @@ import * as nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'hotmail',
   auth: {
-    user: 'rjaz_30@hotmail.com',
+    user: `${process.env.CORREO}`,
     pass: `${process.env.CORREO_PASS}`,
   },
 });
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 // Función para enviar el código de verificación de registro
 export function sendCodeVerification(correoDestino: string, codigo: string) {
   const mailOptions = {
-    from: 'rjaz_30@hotmail.com',
+    from: `${process.env.CORREO}`,
     to: correoDestino,
     subject: 'Código de Verificación',
     text: `Tu código de verificación es: ${codigo}`,
@@ -20,7 +20,6 @@ export function sendCodeVerification(correoDestino: string, codigo: string) {
 
   transporter.sendMail(mailOptions, (error: any, info: { response: any }) => {
     if (error) {
-      console.log(process.env.CORREO_PASS);
       console.error('Error al enviar el correo:', error);
     } else {
       console.log('Correo enviado:', info.response);
@@ -31,7 +30,7 @@ export function sendCodeVerification(correoDestino: string, codigo: string) {
 // Función para enviar el código de verificación de recuperación de contraseña
 export function sendCodeForgotPassword(correoDestino: string, codigo: string) {
   const mailOptions = {
-    from: 'rjaz_30@hotmail.com',
+    from: `${process.env.CORREO}`,
     to: correoDestino,
     subject: 'Código de Verificación',
     text: `Ingresa el siguiente codigo para recuperar tu contraseña: ${codigo}`,
@@ -49,7 +48,7 @@ export function sendCodeForgotPassword(correoDestino: string, codigo: string) {
 // Funcion para reenviar codigo de verificación
 export function resendCodeService(correoDestino: string, codigo: string) {
   const mailOptions = {
-    from: 'rjaz_30@hotmail.com',
+    from: `${process.env.CORREO}`,
     to: correoDestino,
     subject: 'Código de Verificación',
     text: `Tu nuevo codigo es: ${codigo}`,
