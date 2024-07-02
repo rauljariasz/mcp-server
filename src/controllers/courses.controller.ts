ac4bc6fd045325fc2bd5c008aa46212590eff139
rauljariasz <rauljariasz@gmail.com>
2024-07-02 15:26:19 -0400

    CRUD classes

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -56,5 +56,139 @@ export const createCourse = async (
     res.status(201).json({
       data: allCourses,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Hubo un error en la petición.',
+    });
+  }
+};
+
+// Editar curso
+export const editCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id, title, description, level, nameUrl, imageUrl } = req.body;
+
+  try {
+    // *--------------* //
+    // Prevalidaciones //
+    // *------------* //
+    if (!id) {
+      res.status(400).json({
+        message: 'El id es necesario para esta petición',
+      });
+      return;
+    }
+
+    // Validamos si se quiere cambiar el nameUrl
+    if (nameUrl) {
+      const nameUrlAlreadyExist = await courses.findUnique({
+        where: { nameUrl },
+      });
+
+      if (nameUrlAlreadyExist) {
+        res.status(400).json({
+          message: 'El nameUrl ingresado ya existe.',
+        });
+        return;
+      }
+    }
+
+    // Verificamos que el level recibido sea valido
+    if (!Object.values(LEVELS).includes(level)) {
+      res.status(400).json({
+        message: 'El nivel ingresado no es valido.',
+      });
+      return;
+    }
+
+    // *-----* //
+    // Logica //
+    // *---* //
+    const courseToEdit = await courses.findUnique({
+      where: { id },
+    });
+
+    if (!courseToEdit) {
+      res.status(400).json({
+        message: 'No existe un curso con este ID.',
+      });
+      return;
+    }
+
+    await courses.update({
+      where: { id },
+      data: {
+        title: title || courseToEdit?.title,
+        description: description || courseToEdit?.description,
+        level: level || courseToEdit?.level,
+        nameUrl: nameUrl || courseToEdit?.nameUrl,
+        imageUrl: imageUrl || courseToEdit?.imageUrl,
+      },
+    });
+
+    // Respondemos con la lista de cursos para que en el front actualicen el estado de los cursos
+    const rta = await courses.findMany();
+
+    res.status(200).json({
+      message: 'Curso editado correctamente.',
+      data: rta,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Hubo un error en la petición.',
+    });
+  }
+};
+
+// Eliminar curso
+export const deleteCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.body;
+
+  try {
+    // *--------------* //
+    // Prevalidaciones //
+    // *------------* //
+    if (!id) {
+      res.status(400).json({
+        message: 'El id es necesario para esta petición',
+      });
+      return;
+    }
+
+    // *-----* //
+    // Logica //
+    // *---* //
+    const courseToDelete = await courses.findUnique({
+      where: { id },
+    });
+
+    if (!courseToDelete) {
+      res.status(400).json({
+        message: 'No existe un curso con este ID.',
+      });
+      return;
+    }
+
+    await courses.delete({
+      where: { id },
+    });
+
+    const coursesRta = await courses.findMany();
+    res.status(200).json({
+      message: 'Curso eliminado correctamente.',
+      data: coursesRta,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Hubo un error en la petición.',
+    });
+  }
 };
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -2,12 +2,28 @@ import express from 'express';
 import { authenticatedReq } from '../middlewares/auth.handler';
 import { isAdmin } from '../middlewares/admin.handler';
 import { editUserRole, getUser } from '../controllers/admin.controller';
-import { createCourse } from '../controllers/courses.controller';
+import {
+  createCourse,
+  deleteCourse,
+  editCourse,
+} from '../controllers/courses.controller';
+import {
+  createClass,
+  deleteClass,
+  editClass,
+} from '../controllers/classes.controller';
 
 const router = express.Router();
 
 router.get('/getUser', authenticatedReq, isAdmin, getUser);
 router.put('/editUserRole', authenticatedReq, isAdmin, editUserRole);
+// CRUD cursos
 router.post('/createCourse', authenticatedReq, isAdmin, createCourse);
+router.put('/editCourse', authenticatedReq, isAdmin, editCourse);
+router.delete('/deleteCourse', authenticatedReq, isAdmin, deleteCourse);
+// CRUD clases
+router.post('/createClass', authenticatedReq, isAdmin, createClass);
+router.put('/editClass', authenticatedReq, isAdmin, editClass);
+router.delete('/deleteClass', authenticatedReq, isAdmin, deleteClass);
 
 export default router;
